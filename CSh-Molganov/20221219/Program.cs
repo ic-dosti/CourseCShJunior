31ccb4a6b2fc2470d032edb8d698c7d49a871176
078e9 <jb723505@gmail.com>
2022-12-19 08:39:37 +0600

    redisined the function

---
+++ CSh-Molganov/20221219/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20221219
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameBob = "Bob";
+            int numberFirst = 50;
+            float numberB = 100;
+            Console.WriteLine(nameBob);
+            Console.WriteLine(numberFirst);
+            Console.WriteLine(numberB);
+            Console.WriteLine(numberFirst + numberB);
+        }
+    }
+}
